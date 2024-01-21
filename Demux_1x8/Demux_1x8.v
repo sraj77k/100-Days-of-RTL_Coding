d3d4bd7ff4c68625e5992a64153f3c2ca88fd6d5
Saurabh Raj <53854597+sraj77k@users.noreply.github.com>
2024-01-21 08:56:17 +0530

    Update Demux_1x8.v

--- Demux_1x8/Demux_1x8.v
+++ Demux_1x8/Demux_1x8.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.01.2023 19:28:46
-// Design Name: 
-// Module Name: demu_1x8
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+ 
 ///////////////////Behavourial level///////////////////////////////////////////////////////////////
 
 
